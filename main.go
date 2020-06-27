b0df5bf4890af409ea55d911a05272a9afb27f73
masatoshi sugita <mr.f-r.ms621@i.softbank.jp>
2020-06-27 11:53:21 +0900

    first commit

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	//"strconv"
+
+    "github.com/gin-gonic/gin"
+    //"github.com/jinzhu/gorm"
+    _ "github.com/mattn/go-sqlite3"
+)
+
+func main() {
+    router := gin.Default()
+    router.LoadHTMLGlob("templates/*.html")
+
+    router.GET("/", func(ctx *gin.Context){
+        ctx.HTML(200, "index.html", gin.H{})
+    })
+
+    router.Run()
+}
